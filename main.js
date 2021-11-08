b452efddf4e4dcbd3069dc9d63ab412c7059ebba
EmiKusaba <eeemi615@gmail.com>
2021-11-08 06:46:33 -0600

    added firstNum and secondNum func

--- main.js
+++ main.js
@@ -1,62 +1,30 @@
-let firstNum = null ;
-let secondNum = null ;
-let resultNum = null ;
+let firstNum = ""
+let secondNum = ""
+let isFirst = true
+let operation = ""
 
-const saveFirstNumber = (num) => {
-  firstNum = parseInt(num)  
-  console.log(firstNum)
-}
-
-const saveSecondNumber = (num) => {
-  secondNum = parseInt(num)  
-  console.log(secondNum)
-}
-
-const add = (numA, numB) => {
-  const value = numA + numB
-  return value
-}
-
-const subtract = (numA, numB) => {
-  const value = numA - numB
-  return value
-}
-
-const multiply = (numA, numB) => {
-  const value = numA * numB
-  return value
-}
-
-const divide = (numA, numB) => {
-  const value = numA / numB
-  return value
-}
-
-// const modulus = (numA, numB) =>{
-//   const value = numA % numB
-//   // console.log("modulus")
-//   return value
-// }
-const changeOperation = (chosenOperation) => {
-  operation = chosenOperation
-  console.log(operation)
-  return operation
-}
-const putResultInElement = (operationResult) => {
-  console.log(operationResult)
-  document.getElementById("result").value = operationResult
-}
-
-const equals = () => {
-  switch (operation) {
-    case "addition":  putResultInElement(add(firstNum, secondNum)) 
-    break;
-    case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
-    break;
-    case "multiplication": putResultInElement(multiply(firstNum, secondNum))
-    break;
-    case "division": putResultInElement(divide(firstNum, secondNum)) 
-    break;
-    default: "Choose an operation"
+const onNumber = (val) => {
+  if (isFirst) {
+    firstNum += val
+  } else {
+    secondNum += val
   }
+  console.log(val)
+
+  updateScreen()
+}
+
+const onOperation = (val) => {
+  if (firstNum === "") {
+    return
+  }
+  isFirst = false
+  operation = (val)
+  console.log(operation)
+
+  updateScreen()
+}
+
+const updateScreen = () => {
+  document.getElementById("screen").innerHTML = firstNum + " " + operation + " " + secondNum
 }
