b2db01b278fb1cb50e64bfbd4bdc1cec70da88de
EmiKusaba <eeemi615@gmail.com>
2021-11-08 07:42:00 -0600

    added fixed bugs at operatipn and made clearButton

--- main.js
+++ main.js
@@ -31,18 +31,34 @@ const updateScreen = () => {
 
 const onEquals = () => {
   if (firstNum === "" || secondNum === "" || operation === "") {
-    return
+    return screen
   }
-  let screen = getElementById("screen")
+  let screen = document.getElementById("screen")
+  const firstNumInt = parseInt(firstNum)
+  const secondNumInt = parseInt(secondNum)
   switch (operation) {
-    case "+": parseInt(firstNum) + parseInt(secondNum)
-      break;
-    case "-": parseInt(firstNum) - parseInt(secondNum)
-      break;
-    case "x": parseInt(firstNum) * parseInt(secondNum)
-      break;
-    case "/": parseInt(firstNum) / parseInt(secondNum)
-      break;
-    default: "Choose an operation"
+    case "+":
+      screen.innerHTML = firstNumInt + secondNumInt
+      break
+    case "-":
+      screen.innerHTML = firstNumInt - secondNumInt
+      break
+    case "x":
+      screen.innerHTML = firstNumInt * secondNumInt
+      break
+    case "/":
+      screen.innerHTML = firstNumInt / secondNumInt
+      break
+    default:
+      "Choose an operation"
+      break
   }
+}
+
+const onClear = () => {
+  firstNum = ""
+  secondNum = ""
+  isFirst = true
+  operation = ""
+  updateScreen()
 }
